079fc72db6e40b625988d579221140d9265ba1fd
DevExpressExampleBot <fakeEmail>
2021-09-02 16:43:01 +0300

    Source auto update [skip ci]

--- VB/WpfApplication1/Data/DataHelper.vb
+++ VB/WpfApplication1/Data/DataHelper.vb
@@ -4,105 +4,101 @@ Imports System.Linq
 Imports System.Text
 
 Namespace WpfApplication1.Data
-    Public NotInheritable Class DataHelper
+	Public Module DataHelper
+		Public Function GetDummyData() As List(Of Address)
+			Dim countries = New List(Of Country) From {
+				New Country With {
+					.Id = 1,
+					.Name = "USA",
+					.Stats = New PopulationStats With {.CitizensCount = 100}
+				},
+				New Country With {
+					.Id = 2,
+					.Name = "Germany",
+					.Stats = New PopulationStats With {.CitizensCount = 200}
+				},
+				New Country With {
+					.Id = 3,
+					.Name = "Sweden",
+					.Stats = New PopulationStats With {.CitizensCount = 300}
+				},
+				New Country With {
+					.Id = 4,
+					.Name = "Poland",
+					.Stats = New PopulationStats With {.CitizensCount = 400}
+				}
+			}
 
-        Private Sub New()
-        End Sub
+			Dim persons = New List(Of Person) From {
+				New Person With {
+					.Id = 1,
+					.Firstname = "FirstnamePerson1",
+					.Lastname = "LastnamePerson1"
+				},
+				New Person With {
+					.Id = 2,
+					.Firstname = "FirstnamePerson2",
+					.Lastname = "LastnamePerson2"
+				},
+				New Person With {
+					.Id = 3,
+					.Firstname = "FirstnamePerson3",
+					.Lastname = "LastnamePerson3"
+				},
+				New Person With {
+					.Id = 4,
+					.Firstname = "FirstnamePerson4",
+					.Lastname = "LastnamePerson4"
+				},
+				New Person With {
+					.Id = 5,
+					.Firstname = "FirstnamePerson5",
+					.Lastname = "LastnamePerson5"
+				},
+				New Person With {
+					.Id = 6,
+					.Firstname = "FirstnamePerson6",
+					.Lastname = "LastnamePerson6"
+				},
+				New Person With {
+					.Id = 7,
+					.Firstname = "FirstnamePerson7",
+					.Lastname = "LastnamePerson7"
+				},
+				New Person With {
+					.Id = 8,
+					.Firstname = "FirstnamePerson8",
+					.Lastname = "LastnamePerson8"
+				}
+			}
 
-        Public Shared Function GetDummyData() As List(Of Address)
-            Dim countries = New List(Of Country) From { _
-                New Country With { _
-                    .Id = 1, _
-                    .Name = "USA", _
-                    .Stats = New PopulationStats With {.CitizensCount = 100} _
-                }, _
-                New Country With { _
-                    .Id = 2, _
-                    .Name = "Germany", _
-                    .Stats = New PopulationStats With {.CitizensCount = 200} _
-                }, _
-                New Country With { _
-                    .Id = 3, _
-                    .Name = "Sweden", _
-                    .Stats = New PopulationStats With {.CitizensCount = 300} _
-                }, _
-                New Country With { _
-                    .Id = 4, _
-                    .Name = "Poland", _
-                    .Stats = New PopulationStats With {.CitizensCount = 400} _
-                } _
-            }
-
-            Dim persons = New List(Of Person) From { _
-                New Person With { _
-                    .Id = 1, _
-                    .Firstname = "FirstnamePerson1", _
-                    .Lastname = "LastnamePerson1" _
-                }, _
-                New Person With { _
-                    .Id = 2, _
-                    .Firstname = "FirstnamePerson2", _
-                    .Lastname = "LastnamePerson2" _
-                }, _
-                New Person With { _
-                    .Id = 3, _
-                    .Firstname = "FirstnamePerson3", _
-                    .Lastname = "LastnamePerson3" _
-                }, _
-                New Person With { _
-                    .Id = 4, _
-                    .Firstname = "FirstnamePerson4", _
-                    .Lastname = "LastnamePerson4" _
-                }, _
-                New Person With { _
-                    .Id = 5, _
-                    .Firstname = "FirstnamePerson5", _
-                    .Lastname = "LastnamePerson5" _
-                }, _
-                New Person With { _
-                    .Id = 6, _
-                    .Firstname = "FirstnamePerson6", _
-                    .Lastname = "LastnamePerson6" _
-                }, _
-                New Person With { _
-                    .Id = 7, _
-                    .Firstname = "FirstnamePerson7", _
-                    .Lastname = "LastnamePerson7" _
-                }, _
-                New Person With { _
-                    .Id = 8, _
-                    .Firstname = "FirstnamePerson8", _
-                    .Lastname = "LastnamePerson8" _
-                } _
-            }
-
-            Return New List(Of Address) _
-                From { _
-                    New Address With { _
-                        .Number = 1, _
-                        .Name = "Address1", _
-                        .Country = countries(0), _
-                        .Persons = New List(Of Person) From {persons(0), persons(1)} _
-                    }, _
-                    New Address With { _
-                        .Number = 2, _
-                        .Name = "Address2", _
-                        .Country = countries(1), _
-                        .Persons = New List(Of Person) From {persons(2), persons(3)} _
-                    }, _
-                    New Address With { _
-                        .Number = 3, _
-                        .Name = "Address3", _
-                        .Country = countries(2), _
-                        .Persons = New List(Of Person) From {persons(4), persons(5)} _
-                    }, _
-                    New Address With { _
-                        .Number = 4, _
-                        .Name = "Address4", _
-                        .Country = countries(3), _
-                        .Persons = New List(Of Person) From {persons(6), persons(7)} _
-                    } _
-                }
-        End Function
-    End Class
+			Return New List(Of Address) _
+				From {
+					New Address With {
+						.Number = 1,
+						.Name = "Address1",
+						.Country = countries(0),
+						.Persons = New List(Of Person) From {persons(0), persons(1)}
+					},
+					New Address With {
+						.Number = 2,
+						.Name = "Address2",
+						.Country = countries(1),
+						.Persons = New List(Of Person) From {persons(2), persons(3)}
+					},
+					New Address With {
+						.Number = 3,
+						.Name = "Address3",
+						.Country = countries(2),
+						.Persons = New List(Of Person) From {persons(4), persons(5)}
+					},
+					New Address With {
+						.Number = 4,
+						.Name = "Address4",
+						.Country = countries(3),
+						.Persons = New List(Of Person) From {persons(6), persons(7)}
+					}
+				}
+		End Function
+	End Module
 End Namespace
